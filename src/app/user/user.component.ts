d006a69d1831fd0c5db2adddd79949c116846ab6
Eldar Magrafta <eldar.magrafta@motorolasolutioms.com>
2025-11-27 12:13:07 +0200

    video 18 done

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import {DUMMY_USERS} from "../dummy-users";
+import {NgOptimizedImage} from "@angular/common";
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex]
+
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    console.log("onSelectUser Was Called");
+    const idx = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[idx];
+
+  }
+
+
+}
+
