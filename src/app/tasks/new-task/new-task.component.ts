ed0382125959bef7f79e3de7a858d205bfb0a1d5
Eldar Magrafta <eldar.magrafta@motorolasolutioms.com>
2025-12-27 20:52:58 +0200

    03. Angular Essentials - Working with Modules DONE

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,26 +1,23 @@
-import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
-import {FormsModule} from "@angular/forms";
-import {type NewTaskData} from "../task/task.model";
-import {TasksService} from "../tasks.service";
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { type NewTaskData } from "../task/task.model";
+import { TasksService } from "../tasks.service";
 
 @Component({
   selector: 'app-new-task',
-  standalone: true,
-  imports: [
-    FormsModule
-  ],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-  @Input({required:true}) userId!: string;
+  @Input({ required: true }) userId!: string;
   @Output() close = new EventEmitter<void>();
+
   enteredTitle = '';
   enteredSummary = '';
   enteredDueDate = '';
+
   private tasksService = inject(TasksService);
 
-  onCancel(){
+  onCancel() {
     console.log('OnCancel from NewTaskComponent.ts');
     this.close.emit();
   }
@@ -37,6 +34,4 @@ export class NewTaskComponent {
     this.tasksService.addTask(newTsk, this.userId);
     this.close.emit();
   }
-
-
 }
